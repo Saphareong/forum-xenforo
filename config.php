19297cc2b623c74cccdb14361f719f4e39d95022
Tran Hai Name <tranhainam12072001@gmail.com>
2023-05-23 19:57:54 +0700

    Init project

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['db']['host'] = 'localhost';
+$config['db']['port'] = '3306';
+$config['db']['username'] = 'root';
+$config['db']['password'] = '';
+$config['db']['dbname'] = 'forum';
+
+$config['fullUnicode'] = true;
---
+++ internal_data/code_cache/templates/l2/s5/public/extra.less.php
@@ -0,0 +1,35 @@
+<?php
+// FROM HASH: 061e7b925631046c8567f19ff89730b8
+return array('macros' => array(), 'code' => function($__templater, array $__vars)
+{
+	$__finalCompiled = '';
+	$__finalCompiled .= '#imageUpload-1,#fr-image-upload-layer-1 {
+  display:none;
+}
+.sin-img4 {
+    background-image: url(/styles/default/xenforo/sin-img/1.png);
+}
+.user-groups3 {
+    display: inline-block;
+    background: #ff1493;
+    color: #fff;
+    font-weight: 700;
+}
+.user-groups4 {
+    display: inline-block;
+    background: #ff4040;
+    color: #fff;
+    font-weight: 700;
+}
+.bot{
+    background: #7289da;
+    color: #fff;
+}
+.user-groups5 {
+    display: inline-block;
+    background: #3399ff;
+    color: #fff;
+    font-weight: 700;
+}';
+	return $__finalCompiled;
+});
